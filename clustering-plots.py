8462af3eaddc590a970344be0a8d799f0c44c951
Alexandre Lima <alexlimatds@yahoo.com.br>
2019-05-21 16:42:41 -0300

    Addition of subplot and subplotError functions

--- clustering-plots.py
+++ clustering-plots.py
@@ -5,27 +5,37 @@ import matplotlib.ticker as plticker
 
 k = list(range(2, 21))
 
-def plot(methodName, dbPath, crPath, slPath):
+def subplotError(indexName, y_values, errors, sub):
+  sub.errorbar(k, y_values, errors, fmt='-o')
+  sub.set(xlabel='k', ylabel=indexName)
+  sub.margins(x=0.05)
+
+def subplot(indexName, y_values, sub):
+  sub.plot(k, y_values, marker='o')
+  sub.set(xlabel='k', ylabel=indexName)
+  sub.margins(x=0.05)
+
+def plot(methodName, dbPath, crPath, slPath, withError=False):
   fig, (gDB, gCR, gSL) = plt.subplots(nrows=3)
   fig.suptitle(methodName)
 
   dbKmeans = pd.read_csv(dbPath)
-  dbKmeans = dbKmeans[['DB_1', 'DB_2', 'DB_3', 'DB_4', 'DB_5']]
-  gDB.errorbar(k, dbKmeans.mean(axis=1), yerr=dbKmeans.std(axis=1), fmt='-o')
-  gDB.set(xlabel='k', ylabel='DB index')
-  gDB.margins(x=0.05)
-
   crKmeans = pd.read_csv(crPath)
-  crKmeans = crKmeans[['CR_1', 'CR_2', 'CR_3', 'CR_4', 'CR_5']]
-  gCR.errorbar(k, crKmeans.mean(axis=1), yerr=crKmeans.std(axis=1), fmt='-o')
-  gCR.set(xlabel='k', ylabel='CR index')
-  gCR.margins(x=0.05)
-
   slKmeans = pd.read_csv(slPath)
-  slKmeans = slKmeans[['silhouette_1', 'silhouette_2', 'silhouette_3', 'silhouette_4', 'silhouette_5']]
-  gSL.errorbar(k, slKmeans.mean(axis=1), yerr=slKmeans.std(axis=1), fmt='-o')
-  gSL.set(xlabel='k', ylabel='Silhouette index')
-  gSL.margins(x=0.05)
+  
+  if withError:
+    dbKmeans = dbKmeans[['DB_1', 'DB_2', 'DB_3', 'DB_4', 'DB_5']]
+    subplotError('DB index', dbKmeans.mean(axis=1), dbKmeans.std(axis=1), gDB)
+    
+    crKmeans = crKmeans[['CR_1', 'CR_2', 'CR_3', 'CR_4', 'CR_5']]
+    subplotError('CR index', crKmeans.mean(axis=1), crKmeans.std(axis=1), gCR)
+    
+    slKmeans = slKmeans[['silhouette_1', 'silhouette_2', 'silhouette_3', 'silhouette_4', 'silhouette_5']]
+    subplotError('Silhouette index', slKmeans.mean(axis=1), slKmeans.std(axis=1), gSL)
+  else:
+    subplot('DB index', dbKmeans['DB_1'], gDB)
+    subplot('CR index', crKmeans['CR_1'], gCR)
+    subplot('Silhouette index', slKmeans['silhouette_1'], gSL)
 
   loc = plticker.MultipleLocator(base=1.0)
   gDB.xaxis.set_major_locator(loc)
@@ -33,4 +43,6 @@ def plot(methodName, dbPath, crPath, slPath):
   gSL.xaxis.set_major_locator(loc)
   pl.savefig('{}_indexes'.format(methodName))
 
-plot('k-means', 'kmeans-DBs.txt', 'kmeans-CRs.txt', 'kmeans-silhouettes.txt')
+plot('k-means', 'kmeans-DBs.txt', 'kmeans-CRs.txt', 'kmeans-silhouettes.txt', withError=True)
+plot('EM', 'em-DBs.txt', 'em-CRs.txt', 'em-silhouettes.txt', withError=True)
+plot('hierarchical', 'agglomerative-DBs.txt', 'agglomerative-CRs.txt', 'agglomerative-silhouettes.txt')
